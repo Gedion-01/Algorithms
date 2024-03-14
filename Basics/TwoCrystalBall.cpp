40ac76c4ed3696dc2e12f91baa517ff680b508e2
gedion <gedion@domain.com>
2024-03-14 11:22:16 +0300

    up

--- Basics/TwoCrystalBall.cpp
+++ Basics/TwoCrystalBall.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-void bruteforce_two_crystal_ball(int bldg[], int length, int max_floor)
+
+// bruteforce method
+// int two_crystal_balls(int bldg[], int length)
+// {
+//     for(int i = 0; i < length; i++)
+//     {
+//         if(bldg[i]) {
+//             return i;
+//         }
+//     }
+// }
+
+// optimized way
+int two_crystal_balls(bool breaks[], int length)
 {
-    for(int i = 0; i < length; i++)
+    int jmpAmount = sqrt(length);
+    int i = jmpAmount;
+    for(; i < length; i += jmpAmount)
     {
-        if(max_floor == i) {
-            cout << "Max floor: " << i - 1 << endl;
-            return;
+        if(breaks[i])
+        {
+            break;
         }
     }
+
+    i -= jmpAmount;
+    
+    for(int j = 0; j < jmpAmount + 1 && i < length; ++j, ++i)
+    {
+        if(breaks[i])
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 int main()
 {
-    int buildingFloor[6] = {1, 2, 3, 4, 5, 6};
+    bool buildingFloor[41] = {false, false, false, false, false, false, false, false, false, false, 
+    false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false, false, false, false, false, true, true, true, true, 
+    true, true, true, true, true, true, true};
+
+    bool buidingFloor2[6] = {false, false, false, false, true, true};
     
-    bruteforce_two_crystal_ball(buildingFloor, 6, 3);
+    int floor = two_crystal_balls(buidingFloor2, 6);
+    cout << floor << endl;
+
     return 0;
 }
-// function two_crystal_balls(breaks) {
-//   const jmpAmount = Math.floor(Math.sqrt(breaks.length));
-//     console.log(breaks.length)
-//     console.log(jmpAmount)
-//   let i = jmpAmount;
-//   for (; i < breaks.length; i += jmpAmount) {
-//     if (breaks[i]) {
-//       break;
-//     }
-//   }
-//   console.log(i, "i");
-
-//   i -= jmpAmount;
-//   console.log(i, "i");
-
-//   for (let j = 0; j < jmpAmount && i < breaks.length; j++, i++) {
-//     if (breaks[i]) {
-//       console.log(i, "i");
-//       break;
-//     }
-//   }
-//   return -1;
-// }
-
-// console.log(two_crystal_balls([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true]))
-// for(let i = 0; i < 42; i += 6) {
-//     console.log(i)
-// }
