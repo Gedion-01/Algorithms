a963773377771eac3afad1c03d36aaf1756205e1
gedion <gedion@domain.com>
2024-03-17 16:06:09 +0300

    up

--- Advanced/ObjectPool.ts
+++ Advanced/ObjectPool.ts
@@ -2,7 +2,7 @@ class ObjectPool<T> {
     private objects: T[];
     private availableObjects: T[];
 
-    constructor(private createObject: () => T, private resetObjects?: (obj: T) => void, initialSize: number = 10) {
+    constructor(private createObject: () => T, private resetObject?: (obj: T) => void, initialSize: number = 10) {
         this.objects = [];
         this.availableObjects = [];
 
@@ -11,7 +11,27 @@ class ObjectPool<T> {
             this.objects.push(newObj);
             this.availableObjects.push(newObj);
         }
-        console.log(this.objects, this.availableObjects)
+    }
+
+    acquire(): T {
+        if(this.availableObjects.length === 0) {
+            const newObj = this.createObject();
+            this.objects.push(newObj);
+            this.availableObjects.push(newObj);
+        }
+        
+        const obj = this.availableObjects.pop()!;
+        if(this.resetObject) {
+            this.resetObject(obj);
+        }
+        
+        return obj;
+    }
+
+    release(obj: T) {
+
+        this.availableObjects.push(obj);
+        console.log("availableObjects length ",this.availableObjects.length);
     }
     
 }
@@ -23,8 +43,8 @@ class PooledObject {
         this.value = value;
     }
 
-    reset() {
-        // Reset object state if needed
+    reset(): void {
+        // we will reset the object values in here
     }
 
     getValue(): number {
@@ -33,7 +53,22 @@ class PooledObject {
 }
 
 const pool = new ObjectPool<PooledObject>(
-    () => new PooledObject(0),
+    () => new PooledObject(2),
     (obj) => obj.reset(),
     5                          
 );
+// acquiring objects from the pool
+const obj1 = pool.acquire();
+const obj2 = pool.acquire();
+const obj3 = pool.acquire();
+const obj4 = pool.acquire();
+const obj5 = pool.acquire();
+
+// release objects from the pool
+pool.release(obj1);
+pool.release(obj2);
+pool.release(obj3);
+// pool.release(obj4);
+// pool.release(obj5);
+
+console.log(obj5.getValue());
