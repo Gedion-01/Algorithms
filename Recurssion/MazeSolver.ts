f6927e7cfff7fc191e3861d66f0a963909ad50bf
gedion <gedion@domain.com>
2024-03-21 16:31:52 +0300

    update

--- Recurssion/MazeSolver.ts
+++ Recurssion/MazeSolver.ts
@@ -1,19 +1,74 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+type Point = {
+    x: number,
+    y: number
+}
+
+let dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+]
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
     if(curr.x < 0 || curr.x >= maze[0].length ||
-        end.y < 0 || curr.y >= maze.length) {
+        curr.y < 0 || curr.y >= maze.length) { // Corrected line
             return false;
     }
     if(maze[curr.y][curr.x] === wall) {
         return false;
     }
     if(curr.x === end.x && curr.y === end.y) {
+        path.push(end);
         return true;
     }
     if(seen[curr.y][curr.x]) {
         return false;
     }
+
+    // 3 recurs
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr)
+    console.log(curr)
+    
+    // recurse
+    for(let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        console.log("---");
+        console.log("x", curr.x, "+", x)
+        console.log("y", curr.y, "+", y)
+        console.log("---");
+        if(walk(maze, wall, {
+            x: curr.x + x,
+            y: curr.y + y
+        }, end, seen, path)) {
+            return true;
+        }
+    }
+    //post
+    path.pop()
+
+    return false;
 }
 
 function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
 
+    for(let i = 0; i < maze.length; i ++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
 }
+const maze = [
+    'xxxxx x',
+    'x   x x',
+    'x     x',
+    'x xxxxx'
+]
+const p = solve(maze, 'x', {x: 5, y: 0}, {x: 1, y: 3});
+console.log(p);
